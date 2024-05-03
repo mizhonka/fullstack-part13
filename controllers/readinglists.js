1c889c552b76a2bc83982c7faff455b4bcc2f540
Miranda Honkanen <miranda.honkanen@helsinki.fi>
2024-05-03 12:55:57 +0300

    13.22

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 
-const { Entry } = require('../models')
+const { User, Entry } = require('../models')
+
+const { tokenExtractor } = require('../util/middleware')
 
 router.post('/', async (req, res, next) => {
     try {
@@ -9,6 +11,24 @@ router.post('/', async (req, res, next) => {
             userId: req.body.userId,
         })
         return res.json(newEntry)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.put('/:id', tokenExtractor, async (req, res, next) => {
+    try {
+        const blogId = req.params.id
+        const user = await User.findByPk(req.decodedToken.id)
+        const entry = await Entry.findOne({
+            where: {
+                blogId: blogId,
+                userId: user.id,
+            },
+        })
+        entry.read = req.body.read
+        entry.save()
+        return res.json(entry)
     } catch (error) {
         next(error)
     }
